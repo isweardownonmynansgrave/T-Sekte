86aa8547c9a84b0378b05caceea4278b6363d128
isweardownonmynansgrave <148776482+isweardownonmynansgrave@users.noreply.github.com>
2025-10-13 16:49:16 +0200

    Bestellungen weiter ausgearbeitet;

--- Assets/Scripts/Kunde.cs
+++ Assets/Scripts/Kunde.cs
@@ -11,6 +11,8 @@ public class Kunde : MonoBehaviour
 
     // Verwaltung
     public bool DebugMode = false;
+    public int ANZAHL_UNIQUE_REZEPTE = 10;
+    private float timerZuBeginn;
 
     #region Accessoren
     public float timer
@@ -27,12 +29,13 @@ public class Kunde : MonoBehaviour
 
     #region MonoBehaviour
     private void Awake()
-    {
+    { 
         // Zeit-Management
         timer = 0;
 
         // Mood..
         stimmung = 1f;
+
 
         // Bestellungen
         Random rand = new Random();
@@ -46,19 +49,23 @@ public class Kunde : MonoBehaviour
             timer -= Time.deltaTime;
         else
         {
-            if(DebugMode)
-                Debug.Log("Kunde - Timer < 0!!")
+            if (DebugMode)
+                Debug.Log("Kunde - Timer < 0!!");
         }
     }
     #endregion
-
-    public void SetRandomBestellungen()
+    public void SetRandomBestellungen(bool NutzeNudelRezepte = false)
     {
         int[] randomProduktZahlen = new int[anzahlBestellungen];
+        Random rand = new Random();
+        int untergrenzeRandom = NutzeNudelRezepte ? 0 : 1; // Wenn Nudel ja, dann Nudelrezept auf 0 mit einbeziehen, Konzept WIP
+        for (int i = 0; i < anzahlBestellungen; i++)
+            randomProduktZahlen[i] = rand.Next(untergrenzeRandom, ANZAHL_UNIQUE_REZEPTE);
 
-        foreach (int zahl in randomProduktZahlen)
+        for (int i = 0; i < anzahlBestellungen; i++)
         {
             // Random Bestellung auf Basis des integers definieren
+            bestellungen[i] = new Produkt(randomProduktZahlen[i]);
         }
     }
 }
--- Assets/Scripts/Produkt.cs
+++ Assets/Scripts/Produkt.cs
@@ -1,4 +1,17 @@
-public class Produkt
+public class Produkt // Ggf. vererben zu TeeProdukt
 {
-    
+    // Eigenschaften
+    [HideInInspector]
+    public string Name;
+
+    // Gesammelte Infos
+    private string[] namen = ["Nudeln","Name1", "Name2", "usw.."];
+    //private 
+    #region Konstruktor
+    public Produkt(int _typ)
+    {
+        Name = namen[_typ];
+        // Für weitere Properties wiederholen
+    }
+    #endregion
 }
