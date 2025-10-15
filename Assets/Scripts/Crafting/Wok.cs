bc77edb54c2841fded02fe914f61085b5cc16897
isweardownonmynansgrave <148776482+isweardownonmynansgrave@users.noreply.github.com>
2025-10-15 12:24:39 +0200

    Wok crafting implementiert

--- Assets/Scripts/Crafting/Wok.cs
+++ Assets/Scripts/Crafting/Wok.cs
@@ -3,26 +3,58 @@ using UnityEngine;
 public class Wok : MonoBehaviour
 {
     // Crafting
+    [HideInInspector]
     public Produkt Ergebnis;
+    private bool istEssenFertig;
+    private bool istWokBefuellt;
+
 
     // Timing
     public float TimerZuBeginn = 30f;
     [HideInInspector]
     public float Timer;
+    public float TimerAnbrennen;
 
     #region MonoBehaviour
     private void Awake()
     {
-        Timer = TimerZuBeginn;
+        Timer = 0;
+        TimerAnbrennen = 0;
+        Ergebnis = GameManager.ErstelleProdukt(GameManager.Index_Produkt_Nudeln);
+        istEssenFertig = false;
+        istWokBefuellt = false;
     }
     private void Update()
     {
-        if (Timer <= 0)
+        if (istWokBefuellt && !istEssenFertig)
         {
-            // Nudeln fertig - WIP
-        }    
+            if (Timer <= 0)
+            {
+                // Nudeln fertig - WIP
+                istEssenFertig = true;
+                // Weitere Dinge tun?
+            }
+            else
+                Timer -= Time.deltaTime;
+        }
+        else if (istWokBefuellt && istEssenFertig)
+        {
+            TimerAnbrennen += Time.deltaTime;
+        }
         else
-            Timer -= Time.deltaTime;
+        {
+            
+        }
+    }
+    #endregion
+    
+    #region Kochen
+    public void BeginneKochen()
+    {
+        // Ausgangsstatus definieren
+        istWokBefuellt = true;
+        istEssenFertig = false;
+        Timer = TimerZuBeginn;
     }
     #endregion
 }
--- Assets/Scripts/Zutat.cs
+++ Assets/Scripts/Zutat.cs
@@ -1,4 +1,9 @@
 public class Zutat
 {
-    
+    public string Name;
+
+    public Zutat(string _name)
+    {
+        Name = _name;
+    }
 }
